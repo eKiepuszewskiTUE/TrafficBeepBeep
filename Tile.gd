66fffdfc5e6a3bb6a18cce2677250a23a8b45e20
Eric Kiepushevski <e.b.kiepuszewski@student.tue.nl>
2024-01-14 12:36:54 +0100

    (small) Fixed some logic gate textures

--- Tile.gd
+++ Tile.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var road = preload("res://Scenes/road.tscn")
 
+var and_gate = preload("res://Gates/AND.tscn")
+
 var object
 var highlight_sprite : Sprite2D
 
@@ -23,5 +25,6 @@ func _on_area_2d_mouse_exited() -> void:
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and event.pressed):
 		if (object == null):
+			#object = and_gate.instantiate()
 			object = road.instantiate()
 			add_child(object)
