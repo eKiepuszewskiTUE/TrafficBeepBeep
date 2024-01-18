74a13961d835940558b56e69598e1540c6fd6037
CihanGuleryuz <cihanguleryuz2001@gmail.com>
2024-01-18 21:29:12 +0100

    Start the Game scene
    Finish up the start the game scene. There is 2 buttons 1 starts the game 1 goes to tutorial. Waiting for button designs and title to finalize it

---
+++ scripts/title_screen.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_game.tscn") # Replace with function body.
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Tutorial.tscn")
